ad3725c09a008fdcb6738c33b5b6c8950b6b7e53
Subin Shakya <subin12.shakya@gmail.com>
2024-11-13 10:52:50 +0545

    Added more songs

--- setup_database.py
+++ setup_database.py
@@ -17,15 +17,29 @@ CREATE TABLE IF NOT EXISTS songs (
 # Sample songs to insert into the database
 songs = [
     ('Pharrell Williams - Happy', 'happy', 'songs/happy/Pharrell Williams - Happy.mp3'),
+    ('Thats What I Like - Bruno Mars', 'happy', 'songs/happy/Thats What I Like - Bruno Mars.mp3'),
     ('Blue - Yung Kai', 'neutral', 'songs/neutral/Blue - Yung Kai.mp3'),
+    ('Last Night On Earth - Green Day', 'neutral', 'songs/neutral/Last Night On Earth - Green Day.mp3'),    
+    ('RISE - ft. The Glitch Mob, Mako, and The Word Alive', 'neutral', 'songs/neutral/RISE - ft. The Glitch Mob, Mako, and The Word Alive.mp3'),
     ('Maroon 5 Ft. Wiz Khalifa - Payphone', 'neutral', 'songs/neutral/Maroon 5 Ft. Wiz Khalifa - Payphone.mp3'),
+    ('Immortals - Fall Out Boy', 'neutral', 'songs/neutral/Immortals - Fall Out Boy.mp3'),
     ('Harry Styles - Watermelon Sugar', 'happy', 'songs/happy/Harry Styles - Watermelon Sugar.mp3'),
+    ('Jagwar Twin - Happy Face', 'happy', 'songs/happy/Jagwar Twin - Happy Face.mp3'),
     ('Justin Timberlake - CANT STOP THE FEELING!', 'happy', 'songs/happy/Justin Timberlake - CANT STOP THE FEELING!.mp3'),
     ('Eminem - Guts Over Fear ft. Sia', 'fear', 'songs/fear/Eminem - Guts Over Fear ft. Sia.mp3'),
     ('Hanuman Chalisa', 'fear', 'songs/fear/Hanuman Chalisa.mp3'),
     ('Nirvana - Stay Away', 'disgust', 'songs/disgust/Nirvana - Stay Away.mp3'),
+    ('Imagine Dragons x JID - Enemy', 'angry', 'songs/angry/Imagine Dragons x JID - Enemy.mp3'),
+    ('LET THE WORLD BURN - Chris Grey', 'angry', 'songs/angry/LET THE WORLD BURN - Chris Grey.mp3'),
+    ('SAYGRACE - You Dont Own Me ft. G-Eazy', 'angry', 'songs/angry/SAYGRACE - You Dont Own Me ft. G-Eazy.mp3'),
     ('Lola Blanc-Angry Too', 'angry', 'songs/angry/Lola Blanc-Angry Too.mp3'),
     ('Chlöe - Surprise', 'surprise', 'songs/surprise/Chlöe - Surprise.mp3'),
+    ('The Chainsmokers & Coldplay - Something Just Like This', 'surprise', 'songs/surprise/The Chainsmokers & Coldplay - Something Just Like This.mp3'),
+    ('Joji - Die For You', 'sad', 'songs/sad/Joji - Die For You.mp3'),
+    ('Tom Odell - Another Love', 'sad', 'songs/sad/Tom Odell - Another Love.mp3'),
+    ('We The Kings ft. Elena Coats - Sad Song', 'sad', 'songs/sad/We The Kings ft. Elena Coats - Sad Song.mp3'),
+    ('d4vd - Romantic Homicide', 'sad', 'songs/sad/d4vd - Romantic Homicide.mp3'),
+    ('Olivia Rodrigo - Drivers License.mp3', 'sad', 'songs/sad/Olivia Rodrigo - Drivers License.mp3'),
     
 
 ]
